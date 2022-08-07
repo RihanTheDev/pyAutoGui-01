159cd67e8a8f753e9a806363302b9327ffbd6502
Rihanul Islam <rihanthedev@gmail.com>
2022-08-08 04:50:19 +0600

    updated

---
+++ main.py
@@ -0,0 +1,8 @@
+import pyautogui
+    import time
+    while True:
+     time.sleep(3)
+    pyautogui.typewrite('Hello world')
+    pyautogui.press('enter')
+else:
+    print("Wroung Password,Try AGain,Or COntact Us Officils Fb Pae")
